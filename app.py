125538b0644338fb6f3ae5cfe5ff1b81a585caeb
Akhmad Khudri <khudri@binadarma.ac.id>
2025-10-01 08:11:54 +0700

    Adjust iframe bottom padding in app.py

--- app.py
+++ app.py
@@ -169,4 +169,4 @@ def embed_iframe(src, hide_top_px=100, hide_bottom_px=0, height=800):
 
 iframe_url = "https://stc-connect.elpeef.com/"
 
-embed_iframe(iframe_url, hide_top_px=40, hide_bottom_px = -105, height=800)
+embed_iframe(iframe_url, hide_top_px=40, hide_bottom_px = -145, height=800)
