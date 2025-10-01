19daf19c3f22a781c84358cba8597502e111a0ce
Akhmad Khudri <khudri@binadarma.ac.id>
2025-10-01 11:51:09 +0700

    Change page title from 'STC IoT CONNECT' to 'STC CONNECT'

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 st.set_page_config(
-    page_title="STC IoT CONNECT",
+    page_title="STC CONNECT",
     page_icon="🌀",
     layout="wide"
 )
